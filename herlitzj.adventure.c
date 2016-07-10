d146f2953a027b44a55c14d8265a17a6afcc0459
Jeff Herlitz <herlitzj@gmail.com>
2016-07-10 12:27:05 -0700

    rooms saving to disc

--- herlitzj.adventure.c
+++ herlitzj.adventure.c
@@ -79,15 +79,15 @@ void build_connections(struct Room rooms[], int index) {
   }
 }
 
-void create_rooms(char* directory) {
+char* create_rooms(char* directory) {
   printf("Creating rooms\n");
   // Basic variables
-  int i;
-  const BUFFER_SIZE = 100;
-  const NUMBER_OF_ROOMS = 7;
+  int i,j;
+  const int BUFFER_SIZE = 100;
+  const int NUMBER_OF_ROOMS = 7;
 
   // Room generation variables
-  struct Room rooms[7]; // array of structs to hold the room information on init
+  struct Room rooms[10]; // array of structs to hold the room information on init
   char *room_names[10] = {
     "one",
     "two",
@@ -104,7 +104,7 @@ void create_rooms(char* directory) {
   shuffle_rooms(room_names, 10); // random shuffle of room names
 
   // initialize an array of empty Room structs
-  for (i = 0; i < 7; i++) {
+  for (i = 0; i < NUMBER_OF_ROOMS; i++) {
     struct Room tmp;
     tmp.name = room_names[i];
     tmp.type = "MID_ROOM";
@@ -113,35 +113,45 @@ void create_rooms(char* directory) {
   }
 
   // Loop through the array of Room structs and build connections
-  for (i = 0; i < 7; i++) {
+  for (i = 0; i < NUMBER_OF_ROOMS; i++) {
     build_connections(rooms, i);
   }
   printf("CONNECTIONS BUILT\n");
 
-  for (i = 0; i < 7; i++) {
-    printf("NAME: %s\n", rooms[i].name);
-    printf("NUM: %i\n\n", rooms[i].num_of_connections);
+  // choose a start and an end room
+  int start_index = rand() % 7;
+  int end_index = start_index;
+
+  while (start_index == end_index) {
+    end_index = rand() % 7;
   }
 
-/*
+  rooms[start_index].type = "START_ROOM";
+  rooms[end_index].type = "END_ROOM"; 
+
+  // save room structs to disc
   for (i = 0; i < NUMBER_OF_ROOMS; i++) {
     char *file_location = malloc(BUFFER_SIZE);
-    char *file_name = room_names[i];
-    snprintf(file_location, BUFFER_SIZE, "%s/%s.txt", directory, file_name);
+    char *room_name = rooms[i].name;
+    snprintf(file_location, BUFFER_SIZE, "%s/%s.txt", directory, room_name);
     FILE *f = fopen(file_location, "w");
-    fprintf(f, "ROOM NAME: %s\n", file_name);
-    fprintf(f, "CONNECTION:");
-    fprintf(f,"ROOM TYPE: ");
+    fprintf(f, "ROOM NAME: %s\n", room_name);
+    for (j = 0; j < rooms[i].num_of_connections; j++) {
+      fprintf(f, "CONNECTION %i: %s\n", (j + 1), rooms[i].connections[j]);
+    }
+    fprintf(f, "ROOM TYPE: %s\n", rooms[i].type);
     fclose(f);
   }
-*/
+
+  printf("SAVED TO DISC\n");
+  return rooms[start_index].name;
 }
 
 int main() {
   int i;
   char *directory = create_directory();
   printf("DIR: %s\n", directory);
-  create_rooms(directory);
+  char *start_room = create_rooms(directory);
 
   return 0;
 }
