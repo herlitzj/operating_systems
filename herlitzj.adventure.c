31de1351c816ca0e891f9c0341dbfd496b445d7f
Jeff Herlitz <herlitzj@gmail.com>
2016-07-10 09:34:56 -0700

    randomized rooms

--- herlitzj.adventure.c
+++ herlitzj.adventure.c
@@ -4,6 +4,34 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+
+// Room struct to hold all room information during intialization
+struct Room {
+  char *name;
+  char *type;
+  int num_of_connections;
+  char *connections[6];
+};
+
+// Basic swap function for randomization of rooms
+void swap(char *a[], int i, int j) {
+  char *t = a[i];
+  a[i] = a[j];
+  a[j] = t;
+}
+
+// Function for shuffling the array of room names for game initialization
+void shuffle_rooms(char *rooms[], int size) {
+  int i, tmp;
+  srand(time(NULL));
+
+  for (i = (size-1); i > 0; i--) {
+    tmp = rand() % (i + 1);
+    swap(rooms, i, tmp);
+  }
+
+}
 
 char* create_directory() {
   int buffer_size = 25; 
@@ -39,7 +67,7 @@ void create_rooms(char* directory) {
     "ten"
   };
 
-  for(i=0; i<NUMBER_OF_ROOMS; i++) {
+  for (i = 0; i < NUMBER_OF_ROOMS; i++) {
     char *file_location = malloc(BUFFER_SIZE);
     char *file_name = room_names[i];
     snprintf(file_location, BUFFER_SIZE, "%s/%s.txt", directory, file_name);
@@ -53,10 +81,29 @@ void create_rooms(char* directory) {
 }
 
 int main() {
+  int i;
+  //char *directory = create_directory();
+  //printf("DIR: %s\n", directory);
+  //create_rooms(directory);
 
-  char *directory = create_directory();
-  printf("DIR: %s\n", directory);
-  create_rooms(directory);
+  char *room_names[10] = {
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten"
+  };
+
+  shuffle_rooms(room_names, 10);
+
+  for (i = 0; i < 7; i++) {
+    printf("Room: %s\n", room_names[i]);
+  }
 
   return 0;
 }
