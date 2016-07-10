41a304f6c62d985d5aa714530b7dc3440e7d88ca
Jeff Herlitz <herlitzj@gmail.com>
2016-07-10 09:51:39 -0700

    room structs working

--- herlitzj.adventure.c
+++ herlitzj.adventure.c
@@ -33,6 +33,7 @@ void shuffle_rooms(char *rooms[], int size) {
 
 }
 
+// function to create the rooms directory
 char* create_directory() {
   int buffer_size = 25; 
   int pid = getpid(); 
@@ -51,9 +52,13 @@ char* create_directory() {
 
 void create_rooms(char* directory) {
   printf("Creating rooms\n");
+  // Basic variables
   int i;
   const BUFFER_SIZE = 100;
   const NUMBER_OF_ROOMS = 7;
+
+  // Room generation variables
+  struct Room rooms[7]; // array of structs to hold the room information on init
   char *room_names[10] = {
     "one",
     "two",
@@ -67,6 +72,21 @@ void create_rooms(char* directory) {
     "ten"
   };
 
+  shuffle_rooms(room_names, 10); // random shuffle of room names
+
+  for (i = 0; i < 7; i++) {
+    struct Room tmp;
+    tmp.name = room_names[i];
+    tmp.type = "MID_ROOM";
+    tmp.num_of_connections = 0;
+    rooms[i] = tmp;
+  }
+
+  for (i = 0; i < 7; i++) {
+    printf("ROOM: %s\nTYPE: %s\n", rooms[i].name, rooms[i].type);
+  }
+
+/*
   for (i = 0; i < NUMBER_OF_ROOMS; i++) {
     char *file_location = malloc(BUFFER_SIZE);
     char *file_name = room_names[i];
@@ -77,33 +97,14 @@ void create_rooms(char* directory) {
     fprintf(f,"ROOM TYPE: ");
     fclose(f);
   }
-
+*/
 }
 
 int main() {
   int i;
-  //char *directory = create_directory();
-  //printf("DIR: %s\n", directory);
-  //create_rooms(directory);
-
-  char *room_names[10] = {
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten"
-  };
-
-  shuffle_rooms(room_names, 10);
-
-  for (i = 0; i < 7; i++) {
-    printf("Room: %s\n", room_names[i]);
-  }
+  char *directory = create_directory();
+  printf("DIR: %s\n", directory);
+  create_rooms(directory);
 
   return 0;
 }
