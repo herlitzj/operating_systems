6b8f8eb52c685b2d74c266419d385b093a1dc1dc
Jeff Herlitz <herlitzj@gmail.com>
2016-07-30 14:31:18 -0700

    99% complete

--- smallsh.c
+++ smallsh.c
@@ -5,11 +5,7 @@
 #include <signal.h>
 #include <fcntl.h>
 
-// function for handling caught interrupts
-void catch_int(int signo) {
-  printf("terminated by signal: %d\n", signo);
-}
-
+// function for getting the input string from the user
 char *get_input() {
   char *input = NULL;
   ssize_t bufferSize = 0;
@@ -17,17 +13,26 @@ char *get_input() {
   return input;
 }
 
+// function to count the number of args for parsing, etc.
 int count_args(char **args) {
   int i;
   for(i = 0; args[i] != '\0'; i++);
   return i--;
 }
 
+// function for printing the status
 void print_status(int status) {
-  if(status > 1) printf("terminated by signal %i\n", status);
-  else printf("exit status %i\n", status);
+  //printf("STATUS: %i\n", status);
+  //printf("WIFEXITED: %i\n", WIFEXITED(status));
+  //printf("WEXITSTATUS: %i\n", WEXITSTATUS(status));
+  //printf("WIFSIGNALED: %i\n", WIFSIGNALED(status));
+  //printf("WTERMSIG: %i\n", WTERMSIG(status));
+  if(WIFSIGNALED(status)) printf("terminated by signal %i\n", WTERMSIG(status));
+  else if(WIFEXITED(status)) printf("exit value %i\n", WEXITSTATUS(status));
+  fflush(stdout);
 }
 
+// function for parsing the input to tokenize args, etc.
 char **parse_input(char *input) {
   int bufferSize = 100;
   int pos = 0;
@@ -45,6 +50,7 @@ char **parse_input(char *input) {
   return parsedInput;
 }
 
+// function for changing directory when cd command is used
 int change_directory(char **args) {
   if (args[1] == NULL) {
     if(chdir(getenv("HOME")) != 0) perror("smallish");
@@ -54,15 +60,37 @@ int change_directory(char **args) {
   return 0;
 }
 
+// function for getting the index of any key characters in the args
 int get_arg_index(char keyChar, char **args) {
   int i;
   int numberOfArgs = count_args(args);
   for(i = 0; i < numberOfArgs; i++) {
-    if(args[i][0] == keyChar) return i; // return the index of the key character is found
+    if(args[i][0] == keyChar) return i; // return the index of the key character if found
   }
   return -1;
 }
 
+// a helper function for executing child commands
+void execute_child(char *file_args, char *exec_args, int is_output) {
+  int flags = is_output ? O_WRONLY|O_CREAT|O_TRUNC : O_RDONLY;
+  int fd, fd2;
+  fd = open(file_args, flags, 0644);
+  if(fd == -1) {
+    perror("open");
+    fflush(stdout);
+    exit(1);
+  }
+  fd2 = dup2(fd, is_output);
+  if(fd2 == -1) {
+    perror("dup2");
+    exit(1);
+  }
+  fflush(stdout);
+  fflush(stdin);
+  execlp(exec_args, exec_args, NULL);
+}
+
+// execute non built-in commands
 int execute_command(char **args) {
   pid_t pid, wpid;
   int status, fd, fd2;
@@ -72,68 +100,48 @@ int execute_command(char **args) {
 
   if((pid = fork()) < 0) {
     perror("Error forking child process");
+    exit(1);
   } else if (pid == 0) {
+    if(backgroundIndex < 0) signal(SIGINT, SIG_DFL);
     if(outputRedirectIndex > 0) {
-      fd = open(args[outputRedirectIndex + 1], O_WRONLY|O_CREAT|O_TRUNC, 0644);
-      if(fd == -1) {
-        perror("open");
-        exit(1);
-      }
-      fd2 = dup2(fd, 1);
-      if(fd2 == -1) {
-        perror("dup2");
-        exit(2);
-      }
-      fflush(stdout);
-      execlp(args[outputRedirectIndex - 1], args[outputRedirectIndex - 1], NULL);
+      execute_child(args[outputRedirectIndex + 1], args[outputRedirectIndex - 1], 1);
     }
     if(inputRedirectIndex > 0) {
-      fd = open(args[inputRedirectIndex + 1], O_RDONLY, 0644);
-      if(fd == -1) {
-        printf("cannot open %s for input\n", args[inputRedirectIndex + 1]);
-        perror("open");
-        exit(1);
-      }
-      fd2 = dup2(fd, 0);
-      if(fd2 == -1) {
-        perror("dup2");
-        exit(2);
-      }
-      fflush(stdin);
-      execlp(args[inputRedirectIndex - 1], args[inputRedirectIndex - 1], NULL);
+      execute_child(args[inputRedirectIndex + 1], args[inputRedirectIndex - 1], 0);
     }
 
     if(backgroundIndex > 0) {
       args[backgroundIndex] = '\0';
     }
-    printf("outside the if");
-    if(execvp(args[0], args) < 0) {
+
+    pid = execvp(args[0], args);
+    if(pid < 0) {
       perror(args[0]);
       exit(1);
     }
 
   } else {
+    signal(SIGINT, SIG_IGN);
     if(backgroundIndex > 0) {
       wpid = waitpid(pid, &status, WNOHANG);
       printf("background pid is %i\n", pid);
+      fflush(stdout);
     } else {
       do {
         wpid = waitpid(pid, &status, WUNTRACED);
       } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+      if(WIFSIGNALED(status)) print_status(status);
     }
   }
 
-  return 0;
+  return status;
 }
 
-int execute(char **args, int prevStatus) {
+// execute the input
+// check for any built-in commands and execute
+// or call the function for handling non built-in commands
+int execute(char **args, int status) {
   int i;
-  struct sigaction act;
-  act.sa_handler = catch_int;
-  act.sa_flags = 0;
-  sigfillset(&(act.sa_mask));
-
-  sigaction(SIGINT, &act, NULL);
 
   if (args[0] == NULL) {
    return 0;
@@ -142,37 +150,40 @@ int execute(char **args, int prevStatus) {
   if (strcmp(args[0], "cd") == 0) {
     return change_directory(args);
   } else if (strcmp(args[0], "exit") == 0) {
-    return 1;
+    kill(0, SIGTERM);
   } else if (args[0][0] == '#') {
     return 0;
   } else if(strcmp(args[0], "status") == 0) {
-    print_status(prevStatus);
+    print_status(status);
     return 0;
   }
     
   return execute_command(args);
 }
 
+// main function that loops until exit is input
+// checks for background pids and prints status on complete
 int main() {
   char *userInput;
   char **args;
-  int status, pid, i=0;
+  int status, lastChild, pid;
 
   do {
     do {
       pid = waitpid(-1, &status, WNOHANG);
-      if(pid > 0) printf("background pid %i is done: exit status: %i\n", pid, status);
+      if(pid > 0) {
+        printf("background pid %i is done: ", pid);
+        fflush(stdout);
+        print_status(status);
+      }
     } while (pid > 0);
 
     printf(": ");
+    fflush(stdout);
     userInput = get_input();
     args = parse_input(userInput);
-    //if(strcmp(args[0], "exit") == 0) break;
     status = execute(args, status);
-    //free(userInput);
-    //free(args);
-  } while(status != 1);
+  } while(1);
 
-  kill(0, SIGTERM);
   return 0;
 }
