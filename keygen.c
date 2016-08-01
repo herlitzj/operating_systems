9b04ddd5ff148ca11311af416f3f04740cd5a3fd
Jeff Herlitz <herlitzj@gmail.com>
2016-08-01 04:26:09 -0700

    added check for length

--- keygen.c
+++ keygen.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 int main(int argc, char *args[]) {
+  if(args[1] == NULL) { printf("Usage: keygen [length]\n"); return 1; }
   srand(time(NULL));
   int i, random, key_length = atoi(args[1]);
   const int ALPHA_LENGTH = 27;
